fd0e011e03d06650388f55ce2036f83ebf000737
mbeylin <mark_beylin@hotmail.com>
2017-07-08 10:25:24 -0400

    updated contract

--- contracts/GooseHunter.sol
+++ contracts/GooseHunter.sol
@@ -3,22 +3,26 @@ pragma solidity ^0.4.11;
 
 contract GooseHunter {
 
-    struct Goose {
-        bool beingHunted;
+    struct Hunter {
+        address hunter;
         string data;
 
     }
 
-    address[] public gooseHunters;
+    Hunter[] public gooseHunters;
     uint public numGooseHunters;
 
     function GooseHunter() {
 
     }
 
-    function register(){
-        gooseHunters.push(msg.sender);
+    function register(string data){
+        gooseHunters.push(Hunter(msg.sender, data));
         numGooseHunters++;
+    }
+
+    function getHunter(uint id) constant returns (address, string){
+      return (gooseHunters[id].hunter, gooseHunters[id].data);
     }
 
 
